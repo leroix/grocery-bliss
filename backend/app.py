effbf6976792bb4a5a7cca4ad0dacc03edb7dc0a
Justin Ratner <leroix08@gmail.com>
2017-11-06 21:05:45 -0700

    add tests and flask commands to apps.py

--- backend/app.py
+++ backend/app.py
@@ -3,23 +3,41 @@ import os
 import binascii
 
 import connexion
+import click
 
-app = connexion.FlaskApp(
+connexion_app = connexion.FlaskApp(
     __name__,
     specification_dir=os.path.dirname(os.path.realpath(__file__)) + '/swagger/'
 )
 
-app.add_api('swagger.yaml')
+app = connexion_app.app
 
-flask_app = app.app
-
-flask_app.config['GITHUB_CLIENT_ID'] = os.environ['GB_GITHUB_CLIENT_ID']
-flask_app.config['GITHUB_CLIENT_SECRET'] = os.environ['GB_GITHUB_CLIENT_SECRET']
-flask_app.config['SECRET_KEY'] = \
+app.config['GITHUB_CLIENT_ID'] = os.environ['GB_GITHUB_CLIENT_ID']
+app.config['GITHUB_CLIENT_SECRET'] = os.environ['GB_GITHUB_CLIENT_SECRET']
+app.config['SECRET_KEY'] = \
     os.environ.get('GB_SECRET_KEY') or binascii.hexlify(os.urandom(16)).decode('utf8')
 
-if __name__ == '__main__':
-    app.run(port=os.environ.get('GB_PORT', 5000),
+with app.app_context():
+    connexion_app.add_api('swagger.yaml')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app)
+
+@app.cli.command()
+def test():
+    """Run the tests."""
+    import tests
+    tests.run(app)
+    click.echo(click.style('✓ All tests passed', fg='green'))
+
+@app.cli.command()
+def run_connexion():
+    connexion_app.run(port=os.environ.get('GB_PORT', 5000),
             host=os.environ.get('GB_HOST', '0.0.0.0'),
             server=os.environ.get('GB_BACKEND', 'flask'),
             debug=os.environ.get('GB_DEBUG'))
+
+
+if __name__ == '__main__':
+    run_connexion()
