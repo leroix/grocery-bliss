544a4d5c0640e43cb8adcfc0bb73d86005c18522
Justin Ratner <leroix08@gmail.com>
2018-01-21 23:19:13 -0700

    Add comment with test example

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -1,2 +1,21 @@
 def run(app):
+    # Put some tests here. For example,
+
+    #with app.test_client() as c:
+    #    with c.session_transaction() as sess:
+    #        sess['user_id'] = '123'
+
+    #    c.set_cookie('/', 'csrf', 'abc')
+
+    #    create_list_resp = c.post(BASE_URL + '/grocery-lists',
+    #        data=json.dumps({
+    #            'name': 'some list'
+    #        }),
+    #        headers={
+    #            'content-type': 'application/json',
+    #            'X-Csrf': 'abc'
+    #        })
+
+    #    assert create_list_resp.status_code == 201
+
     pass
